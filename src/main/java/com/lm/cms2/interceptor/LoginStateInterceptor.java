0bbc17af507f5d549c3d8c305f0fe98e538c6a48
eyao <2294553196@qq.com>
2019-06-28 22:47:41 +0800

    [news] 添加登录拦截器

---
+++ src/main/java/com/lm/cms2/interceptor/LoginStateInterceptor.java
@@ -0,0 +1,35 @@
+package com.lm.cms2.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginStateInterceptor  implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(true);
+        Object user=session.getAttribute("user");
+        if(user!=null){
+            return true;
+        }else{
+//            不存在则跳转到登录页
+            response.sendRedirect(request.getContextPath()+"/tologin");
+            return false;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
