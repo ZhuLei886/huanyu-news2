4184f29dc02f7d96b59879ea750794fea142deea
eyao <2294553196@qq.com>
2019-06-28 22:58:22 +0800

    [news] 添加邮箱常量

---
+++ src/main/java/com/lm/cms2/util/MyConstant.java
@@ -0,0 +1,9 @@
+package com.lm.cms2.util;
+
+public class MyConstant {
+    // 发送邮件的邮箱，要与application.yml中的一致
+    public static final String MAIL_FROM = "352512524@qq.com";
+    // 域名，需要本地ip ，本地localhost会出现跳转失败状况
+    public static final String DOMAIN_NAME = "http://127.0.0.1:9999/tologin";
+
+}
