e87a5fb857b49d493da05d13662a5ae3e3d0f66c
eyao <2294553196@qq.com>
2019-06-28 23:12:09 +0800

    [news] 添加项目mvc配置

---
+++ src/main/java/com/lm/cms2/config/WebConfiguration.java
@@ -0,0 +1,27 @@
+package com.lm.cms2.config;
+
+import com.huanyunews.news.interceptor.ArticleClickInterceptor;
+import com.huanyunews.news.interceptor.LoginStateInterceptor;
+import com.huanyunews.news.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    private PassportInterceptor passportInterceptor;
+    @Autowired
+    private ArticleClickInterceptor articleClickInterceptor;
+    @Autowired
+    private LoginStateInterceptor loginStateInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor);
+        registry.addInterceptor(articleClickInterceptor).addPathPatterns("/article/*");
+        registry.addInterceptor(loginStateInterceptor).addPathPatterns("/note");
+        super.addInterceptors(registry);
+    }
+}
