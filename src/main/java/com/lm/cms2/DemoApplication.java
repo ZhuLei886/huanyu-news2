c7d4598d21de6303aa117012b8b5c54a8116f420
ZhuLei886 <38902234+ZhuLei886@users.noreply.github.com>
2019-06-28 17:47:36 +0800

    Add files via upload

---
+++ src/main/java/com/lm/cms2/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.lm.cms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
